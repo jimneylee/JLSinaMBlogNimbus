a37c1bb9116b5711be692a87148c7c039de787e3
jimneylee <jimney@163.com>
2013-11-13 16:10:37 +0800

    设置    navi.navigationBar.translucent = NO;

--- SinaMBlog/AppDelegate.m
+++ SinaMBlog/AppDelegate.m
@@ -44,10 +44,11 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     [self prepareForLaunching];
-
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.viewController = [[SMLoginC alloc] initWithStyle:UITableViewStylePlain];
     UINavigationController* navi = [[UINavigationController alloc] initWithRootViewController:self.viewController];
+    navi.navigationBar.translucent = NO;
     self.window.rootViewController = navi;
     [self.window makeKeyAndVisible];
     
