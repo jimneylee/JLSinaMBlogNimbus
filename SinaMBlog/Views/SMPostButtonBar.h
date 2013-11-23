27cf0963858dec2bf0e5c9d2e74f892384299440
jimneylee <jimney@163.com>
2013-11-23 23:17:43 +0800

    按钮条试图，从320修改而来，很不错

---
+++ SinaMBlog/Views/SMPostButtonBar.h
@@ -0,0 +1,12 @@
+//
+//  SMPostButtonBar.h
+//  SinaMBlog
+//
+//  Created by jimney on 13-3-5.
+//  Copyright (c) 2013年 SuperMaxDev. All rights reserved.
+//
+
+#import "TTButtonBar.h"
+@interface SMPostButtonBar : TTButtonBar
+
+@end
---
+++ SinaMBlog/Views/SMPostButtonBar.m
@@ -0,0 +1,41 @@
+//
+//  SMPostButtonBar.m
+//  SinaMBlog
+//
+//  Created by jimney on 13-3-5.
+//  Copyright (c) 2013年 SuperMaxDev. All rights reserved.
+//
+
+#import "SMPostButtonBar.h"
+
+@implementation SMPostButtonBar
+
+- (id)initWithFrame:(CGRect)frame
+{
+	self = [super initWithFrame:frame];
+	if (self) {
+		self.backgroundColor = [UIColor whiteColor];
+	}
+	return self;
+}
+
+- (void)layoutSubviews
+{
+    CGFloat kWidth = 26.f;
+    CGFloat kTopSpace = (self.height - kWidth) * .5f;
+    CGFloat kSpace = (self.width - ( kWidth * _buttons.count)) / (_buttons.count + 1);
+    
+    // 限制间隔距离，最大33.3，按照一排5个布局标准
+    // 33.3 = (320 - 24 * 5）/ (5+1)
+    CGFloat kNormalMaxCount = 5;
+    CGFloat kMaxSpace = (self.width - kWidth * kNormalMaxCount) / (kNormalMaxCount + 1);
+    if (kSpace > kMaxSpace) {
+        kSpace = kMaxSpace;
+    }
+    CGFloat left = kSpace;
+	for (UIButton* button in _buttons) {
+		button.frame = CGRectMake(left, kTopSpace, kWidth, kWidth);
+		left = button.right + kSpace;
+	}
+}
+@end
