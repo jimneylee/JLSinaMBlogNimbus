022c6c07bf474f9b09ae7494c1569e9862fbc351
jimneylee <jimney@163.com>
2013-11-23 23:08:24 +0800

    添加发布微博和评论微博的接口

--- SinaMBlog/Common/SMAPIClient.h
+++ SinaMBlog/Common/SMAPIClient.h
@@ -29,6 +29,33 @@
 // @我的微博:maxId
 + (NSString*)relativePathForAtMeTimelineWithMaxId:(NSString *)maxId;
 
+// 获取话题列表数据
++ (NSString*)relativePathForWeeklyTrendsList;
+
+//================================================================================
+// status write
+//================================================================================
+// 发送文字微博
++ (NSString*)relativePathForPostTextStatus;
+
+// 发送图片文字微博
++ (NSString*)relativePathForPostImageStatus;
+
+// 转发微博
++ (NSString*)relativePathForRepostStatus;
+
+// 删除微博
++ (NSString*)relativePathForDestroyStatus;
+
+//================================================================================
+// comment wirte
+//================================================================================
+// 发一条微博评论
++ (NSString*)relativePathForCreateComment;
+
+// 删除一条微博评论
++ (NSString*)relativePathForDestroyComment;
+
 @end
 
 NSString *const kSNAPIBaseURLString;
