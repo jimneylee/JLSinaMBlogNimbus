2542167fb289ebeb3da8f62a5065d42dd08c2e4e
jimneylee <jimney@163.com>
2013-11-23 23:18:03 +0800

    列表排序

---
+++ SinaMBlog/Entities/SMSectionItemBaseEntity.h
@@ -0,0 +1,21 @@
+//
+//  SMSectionItemEntity.h
+//  SinaMBlog
+//
+//  Created by jimney on 13-3-12.
+//  Copyright (c) 2013年 SuperMaxDev. All rights reserved.
+//
+
+@interface SMSectionItemBaseEntity : NISubtitleCellObject
+{
+    NSString* _name;
+    unichar _firstLetter;  // 首字母
+    NSString* _sortString;  // 供排序所用字串
+}
+
+- (NSString* )createSortString;
+
+@property (nonatomic, copy) NSString* name;
+@property (nonatomic, copy) NSString* sortString;
+@property (nonatomic) unichar firstLetter;
+@end
---
+++ SinaMBlog/Entities/SMSectionListBaseEntity.h
@@ -0,0 +1,23 @@
+//
+//  SMSectionListBaseEntity.h
+//  SinaMBlog
+//
+//  Created by jimney on 13-3-12.
+//  Copyright (c) 2013年 SuperMaxDev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMSectionListBaseEntity : NSObject
+{
+    NSMutableArray* _unsortedArray;
+    NSMutableArray* _sections;
+    NSMutableArray* _items;
+}
+
+- (void)sort;
+
+@property (nonatomic, retain) NSMutableArray* unsortedArray;
+@property (nonatomic, retain) NSMutableArray* sections;
+@property (nonatomic, retain) NSMutableArray* items;
+@end
