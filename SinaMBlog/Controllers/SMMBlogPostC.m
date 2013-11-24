4bec5b77823400b0ed02a2184cdf5fc8c9191a1b
jimneylee <jimney@163.com>
2013-11-24 15:01:15 +0800

    copyright change

--- SinaMBlog/Controllers/SMMBlogPostC.m
+++ SinaMBlog/Controllers/SMMBlogPostC.m
@@ -3,7 +3,7 @@
 //  SinaMBlog
 //
 //  Created by jimney on 13-3-4.
-//  Copyright (c) 2013年 SuperMaxDev. All rights reserved.
+//  Copyright (c) 2013年 jimneylee. All rights reserved.
 //
 
 #import "SMMBlogPostC.h"
