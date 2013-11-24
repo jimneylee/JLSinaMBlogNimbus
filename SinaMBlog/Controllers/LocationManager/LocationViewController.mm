9c5c7e5f4a39eafaa5371eedf2ca37089b83eb05
jimneylee <jimney@163.com>
2013-11-24 14:58:54 +0800

    update

--- SinaMBlog/Controllers/LocationManager/LocationViewController.mm
+++ SinaMBlog/Controllers/LocationManager/LocationViewController.mm
@@ -3,7 +3,7 @@
 //  MapKitTest
 //
 //  Created by jimney on 13-3-20.
-//  Copyright (c) 2013年 SuperMaxDev. All rights reserved.
+//  Copyright (c) 2013年 jimneylee. All rights reserved.
 //
 
 #import "LocationViewController.h"
