7b5645406e870f687d5e2c6b784b01163f6a0ccc
jimneylee <jimney@163.com>
2013-11-24 15:02:50 +0800

    完善好解析

--- SinaMBlog/Entities/SMTrendListEntity.h
+++ SinaMBlog/Entities/SMTrendListEntity.h
@@ -3,15 +3,14 @@
 //  SinaMBlog
 //
 //  Created by jimney on 13-3-8.
-//  Copyright (c) 2013年 SuperMaxDev. All rights reserved.
+//  Copyright (c) 2013年 jimneylee. All rights reserved.
 //
 
 #import "SMSectionListBaseEntity.h"
 
-@interface SMTrendsEntity : SMSectionListBaseEntity
-{
-    NSString* _time;
-}
+@interface SMTrendListEntity : SMSectionListBaseEntity
 
 @property (nonatomic, copy) NSString* time;
++ (id)entityWithDictionary:(NSDictionary*)dic;
+
 @end
--- SinaMBlog/Entities/SMTrendListEntity.m
+++ SinaMBlog/Entities/SMTrendListEntity.m
@@ -3,21 +3,37 @@
 //  SinaMBlog
 //
 //  Created by jimney on 13-3-8.
-//  Copyright (c) 2013年 SuperMaxDev. All rights reserved.
+//  Copyright (c) 2013年 jimneylee. All rights reserved.
 //
 
-#import "SMTrendsEntity.h"
+#import "SMTrendListEntity.h"
 #import "SMTrendEntity.h"
 #import "pinyin.h"
 
-@implementation SMTrendsEntity
-@synthesize time = _time;
-
+@implementation SMTrendListEntity
+//{
+//    "as_of" = 1385257540;
+//    trends =     {
+//        "2013-11-24 09:45" =         (
+//                                      {
+//                                          amount = 30213;
+//                                          delta = 30213;
+//                                          name = "\U90ed\U4e66\U7476";
+//                                          query = "\U90ed\U4e66\U7476";
+//                                      },
+//                                      {
+//                                          amount = 27045;
+//                                          delta = 27045;
+//                                          name = "\U91d1\U9a6c\U5956";
+//                                          query = "\U91d1\U9a6c\U5956";
+//                                      })
+//    }
+//}
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-+ (id)createWithDictionary:(NSDictionary*)dic
++ (id)entityWithDictionary:(NSDictionary*)dic
 {
-    SMTrendsEntity* entity = [SMTrendsEntity new];
-    NSDictionary* sourceDic = [dic objectForKey:@"trends"];
+    SMTrendListEntity* entity = [SMTrendListEntity new];
+    NSDictionary* sourceDic = [dic objectForKey:JSON_TREND_LIST];
     NSArray* keys = [sourceDic allKeys];
     if (keys.count > 0) {
         NSString* key = [keys objectAtIndex:0];
@@ -40,7 +56,7 @@
         
         entity.unsortedArray = [NSMutableArray arrayWithCapacity:sourceArray.count];
         for (NSDictionary* d in sourceArray) {
-            SMTrendEntity* aTrend = [SMTrendEntity createWithDictionary:d];
+            SMTrendEntity* aTrend = [SMTrendEntity entityWithDictionary:d];
             if (aTrend) {
                 [entity.unsortedArray addObject:aTrend];
             }
