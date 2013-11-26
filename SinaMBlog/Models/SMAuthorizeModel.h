39bdded7759b34c5d23029af119f91d13a3ffd44
jimneylee <jimney@163.com>
2013-11-26 22:17:24 +0800

    从Login控制器剥离出授权model，更合理。

--- SinaMBlog/Models/SMAuthorizeModel.h
+++ SinaMBlog/Models/SMAuthorizeModel.h
@@ -8,6 +8,9 @@
 
 #import <Foundation/Foundation.h>
 
-@interface SMUserModel : NSObject
+@interface SMAuthorizeModel : NSObject
+
++ (void)sendAuthorizeRequest;
++ (BOOL)isAuthorized;
 
 @end
--- SinaMBlog/Models/SMAuthorizeModel.m
+++ SinaMBlog/Models/SMAuthorizeModel.m
@@ -6,8 +6,53 @@
 //  Copyright (c) 2013 SuperMaxDev. All rights reserved.
 //
 
-#import "SMUserModel.h"
+#import "SMAuthorizeModel.h"
 
-@implementation SMUserModel
+@implementation SMAuthorizeModel
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - Authorization
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
++ (void)sendAuthorizeRequest
+{
+    WBAuthorizeRequest *request = [WBAuthorizeRequest request];
+    request.redirectURI = SinaWeiboV2RedirectUri;
+    request.scope = nil;//http://open.weibo.com/wiki/Scope
+    request.userInfo = nil;
+    [WeiboSDK sendRequest:request];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// TODO:打算从这个类中剥离出来，放这里有点不合适
+///////////////////////////////////////////////////////////////////////////////////////////////////
++ (BOOL)isAuthorized
+{
+	return [SMGlobalConfig getCurrentLoginedUserId]
+    && [SMGlobalConfig getCurrentLoginedAccessToken]
+    && ![self isAuthorizeExpired];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
++ (BOOL)isAuthorizeExpired
+{
+    if ([SMGlobalConfig getCurrentLoginedExpiresIn]) {
+        
+        NSDate* expirationDate = [NSDate distantPast];
+        
+        if ([SMGlobalConfig getCurrentLoginedExpiresIn]) {
+            int expVal = [[SMGlobalConfig getCurrentLoginedExpiresIn] intValue];
+            if (expVal != 0) {
+                expirationDate = [NSDate dateWithTimeIntervalSinceNow:expVal];
+            }
+        }
+        
+        NSDate* now = [NSDate date];
+        return ([now compare:expirationDate] == NSOrderedDescending);
+    }
+    
+    return NO;
+}
 
 @end
