8b6d75d9c0e5b36fd1c61bd1dcfac54ce8ee7a4b
jimneylee <jimney@163.com>
2013-12-04 10:11:40 +0800

    统一注释格式

--- SinaMBlog/Common/SMAPIClient.m
+++ SinaMBlog/Common/SMAPIClient.m
@@ -102,7 +102,7 @@ NSString *const kSNAPIBaseURLString = @"https://api.weibo.com/2/";
 // 当前登录用户及关注好友微博某页
 + (NSString*)relativePathForFriendsTimelineWithMaxId:(NSString *)maxId
 {
-    return [NSString stringWithFormat:@"statuses/friends_timeline.json?max_id=%@&access_token=%@",//todo:count default 20[200]
+    return [NSString stringWithFormat:@"statuses/friends_timeline.json?max_id=%@&access_token=%@",
             maxId, [SMGlobalConfig getCurrentLoginedAccessToken]];
 }
 
@@ -120,81 +120,50 @@ NSString *const kSNAPIBaseURLString = @"https://api.weibo.com/2/";
             maxId, [SMGlobalConfig getCurrentLoginedAccessToken]];
 }
 
-#pragma mark -
-#pragma mark Trends
-/**
- * 话题列表数据
- * 参考：http://open.weibo.com/wiki/Trends/weekly
- * 请求方式: GET
- */
+#pragma mark - Trends
+
+// 话题列表数据
 + (NSString*)relativePathForWeeklyTrendsList
 {
     return [NSString stringWithFormat:@"trends/weekly.json?access_token=%@",
             [SMGlobalConfig getCurrentLoginedAccessToken]];
 }
-#pragma mark MBlog write
-/**
- * 发送文字微博
- * 参考：open.weibo.com/wiki/index/Statuses/update
- * status:微博文字
- * 请求方式: POST
- */
+
+#pragma mark - MBlog write
+
+// 发送文字微博
 + (NSString*)relativePathForPostTextStatus
 {
     return [NSString stringWithFormat:@"statuses/update.json"];
     
 }
 
-/**
- * 发送图片文字微博
- * 参考：open.weibo.com/wiki/Statuses/upload
- * status:微博文字
- * pic: 微博图片
- * 请求方式: POST
- */
+// 发送图片文字微博
 + (NSString*)relativePathForPostImageStatus
 {
     return [NSString stringWithFormat:@"statuses/upload.json"];
 }
 
-/**
- * 转发微博
- * 参考：open.weibo.com/wiki/Statuses/repost
- * status:转发微博文字
- * id: 要转发的微博ID
- * is_comment: 是否在转发的同时发表评论。0表示不发表评论，1表示发表评论给当前微博，2表示发表评论给原微博，3是1、2都发表。默认为0。
- * 请求方式: POST
- */
+// 转发微博
 + (NSString*)relativePathForRepostStatus
 {
     return [NSString stringWithFormat:@"statuses/repost.json"];
 }
 
-/**
- * 删除微博
- * 参考：http://open.weibo.com/wiki/2/statuses/destroy
- * 请求方式: POST
- */
+// 删除微博
 + (NSString*)relativePathForDestroyStatus
 {
     return [NSString stringWithFormat:@"statuses/destroy.json"];
 }
 
-#pragma mark Comment write
-/**
- * 评论微博
- * 参考：open.weibo.com/wiki/Statuses/repost
- * id: 要评论的微博消息ID
- * comment: 评论的内容
- * cid: 要回复的评论id
- * without_mention: 1：回复中不自动加入“回复@用户名”，0：回复中自动加入“回复@用户名”.默认为0.
- * comment_ori: 当评论一条转发微博时，是否评论给原微博。0:不评论给原微博。1：评论给原微博。默认0.
- */
+#pragma mark - Comment write
+// 评论微博
 + (NSString*)relativePathForCreateComment
 {
     return [NSString stringWithFormat:@"comments/create.json"];
 }
 
+// 删除评论
 + (NSString*)relativePathForDestroyComment
 {
     return [NSString stringWithFormat:@"comments/destroy.json"];
