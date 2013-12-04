36eee2f5412b63b2570c3474e299284c55933d1a
jimneylee <jimney@163.com>
2013-12-04 14:34:56 +0800

    需要高级接口支持

---
+++ SinaMBlog/Controllers/SMUserHomeC.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SinaMBlogNimbus
+//
+//  Created by jimneylee on 13-10-30.
+//  Copyright (c) 2013年 jimneylee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "LJJBaseTableC.h"
+
+@interface SMUserHomeC : LJJBaseTableC
+
+@end
