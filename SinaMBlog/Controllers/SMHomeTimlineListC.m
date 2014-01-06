059c6b7777a51e472e027260cb41ff713cd6b571
jimneylee <jimneylee@gmail.com>
2014-01-06 09:09:27 +0800

    set backgournd of tableview

--- SinaMBlog/Controllers/SMHomeTimlineListC.m
+++ SinaMBlog/Controllers/SMHomeTimlineListC.m
@@ -45,7 +45,10 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
+    
+    self.tableView.separatorColor = [UIColor clearColor];
+    self.tableView.backgroundColor = TABLE_VIEW_BG_COLOR;
+    self.tableView.backgroundView = nil;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
