5fe2a1ac519e89f56f21aa47d9a1ce12c7fc5880
jimneylee <jimneylee@gmail.com>
2014-11-23 11:51:08 +0800

    update

--- SinaMBlog/Entities/SMUserInfoEntity.h
+++ SinaMBlog/Entities/SMUserInfoEntity.h
@@ -14,35 +14,7 @@
 
 @property(nonatomic, retain) SMStatusEntity *status;
 @property(nonatomic, copy) NSString *userID;
-@property(nonatomic, copy) NSString *idstr;
-@property(nonatomic, copy) NSString *screen_name;
+@property(nonatomic, copy) NSString *screenName;
 @property(nonatomic, copy) NSString *name;
-@property(assign) int province;
-@property(assign) int city;
-@property(nonatomic, copy) NSString *location;
-@property(nonatomic, copy) NSString *description;
-@property(nonatomic, copy) NSString *url;
-@property(nonatomic, copy) NSString *profile_image_url;
-@property(nonatomic, copy) NSString *profile_url;
-@property(nonatomic, copy) NSString *domain;
-@property(nonatomic, copy) NSString *weihao;
-@property(nonatomic, copy) NSString *gender;
-@property(assign) int followers_count;
-@property(assign) int friends_count;
-@property(assign) int statuses_count;
-@property(assign) int favourites_count;
-@property(nonatomic, copy) NSString *created_at;
-@property(assign) BOOL following;
-@property(assign) BOOL allow_all_act_msg;
-@property(assign) BOOL geo_enabled;
-@property(assign) BOOL verified;
-@property(nonatomic, copy) NSString *remark;
-@property(assign) BOOL allow_all_comment;
-@property(nonatomic, copy) NSString *avatar_large;
-@property(nonatomic, copy) NSString *verified_reason;
-@property(assign) BOOL follow_me;
-@property(assign) int online_status;
-@property(assign) int bi_followers_count;
-@property(nonatomic, copy) NSString *lang;
-
+@property(nonatomic, copy) NSString *profileImageUrl;
 @end
--- SinaMBlog/Entities/SMUserInfoEntity.m
+++ SinaMBlog/Entities/SMUserInfoEntity.m
@@ -16,40 +16,20 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (id)initWithDictionary:(NSDictionary*)dic
 {
-    if (!dic.count || ![dic isKindOfClass:[NSDictionary class]]) {
-        return nil;
-    }
-    
     self = [super initWithDictionary:dic];
     if (self) {
         self.status   = (SMStatusEntity *)[SMStatusEntity entityWithDictionary:dic[JSON_STATUS]];
         self.userID = [NSString getStringValue:dic[JSON_USERINFO_USERID]];
-        self.idstr = dic[JSON_USERINFO_IDSTR];
-        self.screen_name = dic[JSON_USERINFO_SCREEN_NAME];
+        self.screenName = dic[JSON_USERINFO_SCREEN_NAME];
         self.name = dic[JSON_USERINFO_NAME];
-        self.gender = dic[JSON_USERINFO_GENDER];
-        self.province = [dic[JSON_USERINFO_PROVINCE] intValue];
-        self.location = dic[JSON_USERINFO_LOCATION];
-        self.description = dic[JSON_USERINFO_DESCRIPTION];
-        self.url = dic[JSON_USERINFO_URL];
-        self.profile_image_url = dic[JSON_USERINFO_PROFILE_IMAGE_URL];
-        self.profile_url = dic[JSON_USERINFO_PROFILE_URL];
-        self.followers_count = [dic[JSON_USERINFO_FOLLOWERS_COUNT] intValue];
-        self.friends_count = [dic[JSON_USERINFO_FRIENDS_COUNT] intValue];
-        self.statuses_count = [dic[JSON_USERINFO_STATUSES_COUNT] intValue];
-        self.favourites_count = [dic[JSON_USERINFO_FAVOURITES_COUNT] intValue];
-        self.following = [dic[JSON_USERINFO_FOLLOWING] boolValue];
-        self.follow_me = [dic[JSON_USERINFO_FOLLOW_ME] boolValue];
+        self.profileImageUrl = dic[JSON_USERINFO_PROFILE_IMAGE_URL];
     }
     return self;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-+ (JLNimbusEntity *)entityWithDictionary:(NSDictionary *)dic {
-    if (!dic.count || ![dic isKindOfClass:[NSDictionary class]]) {
-        return nil;
-    }
-    
++ (JLNimbusEntity *)entityWithDictionary:(NSDictionary *)dic
+{
     SMUserInfoEntity *entity = [[SMUserInfoEntity alloc] initWithDictionary:dic];   
     return entity;
 }
